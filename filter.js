121d7d5e155ba82ee1984f83ab30bccf1cac96a7
garyForeman <gforema2@illinois.edu>
2014-07-28 13:22:25 -0500

    Added comments and improved regex.

--- filter.js
+++ filter.js
@@ -1,6 +1,9 @@
-var floatRegex = '[0-9]+[,]?[0-9]*[.][0-9]*|[0-9]+[,]?[0-9]*'
+var floatRegex = /\d+,?\d*(\.\d*)?/
 
 var getInput = function(promptString){
+    //Prompts user to enter a price than can be converted to a float. Returns
+    //the value of the input converted to a float. Handles incorrect input
+    // by asking the user for a new entry.
     var acceptableInput =  false;
     while(!acceptableInput){
 	var priceString = prompt(promptString).match(floatRegex)[0]
@@ -16,17 +19,32 @@ var getInput = function(promptString){
     return priceVar;
 };
 
-priceLow = getInput("Enter the lowest price you wish to see.");
-priceHigh = getInput("Enter the highest price you wish to see.");
+var getPrices = true
 
-var prices = $('.pairsInline').find('span').contents()
+while(getPrices){
+    priceLow = getInput("Enter the lowest price you wish to see.");
+    priceHigh = getInput("Enter the highest price you wish to see.");
+    if(priceLow > priceHigh){
+	alert("Your low price was greater than your high price!");
+    }
+    else{
+	getPrices = false;
+    };
+};
+
+//class pairsInline indicates the line under the thread titles that
+//contain the Price and Location information. The price itself is located in
+//the only 'span' decendent of pairsInline 
+var prices = $('.pairsInline').find('span').contents();
 
 for(var i = 0; i < prices.length; i++){
     price = prices[i].data.match(floatRegex);
     if(price){
 	price = parseFloat(price[0].replace(",",""));
 	if(price < priceLow || price > priceHigh){
+	    //each thread is indicated by an 'li' selector, 'li' is not used for
+	    //any other purpose
 	    $(prices[i]).parents('li').remove()
 	};
     };
-}
+};
