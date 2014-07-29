9cdebc561a6d6ccb2d1f2f0fbfa92484bee035e7
garyForeman <gforema2@illinois.edu>
2014-07-29 12:55:29 -0500

    Began work on the hard filter.

--- filter.js
+++ filter.js
@@ -1,4 +1,4 @@
-var floatRegex = /\d+,?\d*(\.\d*)?/
+var floatRegex = /\d+,?\d*(\.\d*)?/;
 
 var getInput = function(promptString){
     //Prompts user to enter a price than can be converted to a float. Returns
@@ -10,7 +10,7 @@ var getInput = function(promptString){
 	    .replace(",", "");
 	var priceVar = parseFloat(priceString);
 	if (isNaN(priceVar)){
-	    alert("Invalid input!")
+	    alert("Invalid input!");
 	}
 	else{
 	    acceptableInput = true;
@@ -19,7 +19,35 @@ var getInput = function(promptString){
     return priceVar;
 };
 
-var getPrices = true
+var filterType = function(){
+    //Asks the user whether she would like a hard or soft filter to be used.
+    //The hard filter eliminates threads without listed prices and threads
+    //with prices that cannot be converted to floats. The soft filter keeeps
+    //these threads. Returns true if hard filter is chosen or false if soft
+    //filter is chosen
+    var acceptableInput = false;
+    var hardFilter = false;
+    while(!acceptableInput){
+	var filterString = prompt("Would you like to use a HARD or " +
+				  "SOFT filter?").toUpperCase();
+	switch(filterString){
+	    case "HARD":
+	        hardFilter = true;
+	        acceptableInput = true;
+	        break;
+	    case "SOFT":
+	        hardFilter = false;
+	        acceptableInput = true;
+                break;
+            default:
+	        alert("Invalid input!");
+	        break;
+	};
+    };
+    return hardFilter;
+};
+
+var getPrices = true;
 
 while(getPrices){
     priceLow = getInput("Enter the lowest price you wish to see.");
@@ -31,6 +59,8 @@ while(getPrices){
 	getPrices = false;
     };
 };
+
+var hardFilter = filterType();
 
 //class pairsInline indicates the line under the thread titles that
 //contain the Price and Location information. The price itself is located in
@@ -44,7 +74,7 @@ for(var i = 0; i < prices.length; i++){
 	if(price < priceLow || price > priceHigh){
 	    //each thread is indicated by an 'li' selector, 'li' is not used for
 	    //any other purpose
-	    $(prices[i]).parents('li').remove()
+	    $(prices[i]).parents('li').remove();
 	};
     };
 };
